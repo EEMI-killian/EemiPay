39c5a135b713926009e32cd4e06e56f7fefa8854
killian <killian.angely@outlook.fr>
2025-07-20 13:44:12 +0200

    migration table payment_methods

---
+++ api-platform/src/migration/1753011595567-migration.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1753011595567 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE public.payment_methods (
+                "id" character varying NOT NULL,
+                "card_holder_name" character varying NOT NULL,
+                "card_number" character varying NOT NULL,
+                "expiry_date" character varying NOT NULL,
+                "cvv" character varying NOT NULL,
+                "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                CONSTRAINT "PK_payment_methods_id" PRIMARY KEY ("id")
+            )
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE public.payment_methods
+        `);
+    }
+
+}
